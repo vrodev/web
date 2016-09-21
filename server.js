3e35386ac374b7d169d068a5bdef33949211a675
Emma Nergell <emma.nergell@gmail.com>
2016-09-21 12:48:20 +0200

    Html-saker

--- server.js
+++ server.js
@@ -5,11 +5,15 @@ var app = express()
 app.use(logger('dev'))
 app.use(express.static(__dirname + '/static'))
 app.set('views', __dirname + '/source/templates')
-app.locals.pretty = true;
+app.locals.pretty = false;
 app.set('view engine', 'jade')
 
 app.get('/', function (req, res) {
   res.render("homepage", {title:'home'})
+})
+
+app.get('/a', function (req, res) {
+  res.render("divfishpage", {title:'home'})
 })
 
 app.listen(process.env.PORT || 3000, function () {
--- static/js/index.js
+++ static/js/index.js
@@ -2,3 +2,79 @@
 // VRO Web
 //
 // Initially by Leonard Pauli, sep 2016
+
+
+/*var ettplusKlickad = function() {
+	var sill= document.body.querySelector(".abborre")
+	sill.classList.add("icansee")
+
+	var sill= document.body.querySelector(".sill")
+	sill.classList.remove("icansee")
+
+	var sill= document.body.querySelector(".torsk")
+	sill.classList.remove("icansee")
+
+}
+
+var nolladKlickad = function() {
+	var sill= document.body.querySelector(".sill")
+	sill.classList.add("icansee")
+
+	var sill= document.body.querySelector(".abborre")
+	sill.classList.remove("icansee")
+
+	var sill= document.body.querySelector(".torsk")
+	sill.classList.remove("icansee")
+}
+
+var faqKlickad= function() {
+	var sill= document.body.querySelector(".torsk")
+	sill.classList.add("icansee")
+
+	var sill= document.body.querySelector(".abborre")
+	sill.classList.remove("icansee")
+
+	var sill= document.body.querySelector(".sill")
+	sill.classList.remove("icansee")
+
+
+}
+
+
+window.onload=function()
+{
+	var nollad = document.body.querySelector(".ettplus")
+	nollad.classList.add("fish")
+	nollad.addEventListener("click", ettplusKlickad)
+
+	var nollad = document.body.querySelector(".nollad")
+	nollad.classList.add("fish")
+	nollad.addEventListener("click", nolladKlickad)
+
+	var nollad = document.body.querySelector(".faq")
+	nollad.classList.add("fish")
+	nollad.addEventListener("click", faqKlickad)
+
+	
+
+}	*/
+	
+
+window.onload=function(){
+	var classToElementMapFn = function(className) {
+	return document.body.querySelector('.'+className)
+}
+var buttons = "ettplus,nollad,faq".split(',').map(classToElementMapFn)
+var pages = "abborre,sill,torsk".split(',').map(classToElementMapFn)
+
+var addClickListenerToButton = function(button, buttonIndex) {
+	button.classList.add("fish")
+	button.addEventListener('click', function () {
+		pages.map(function (page, pageIndex) {
+			var show = pageIndex == buttonIndex
+			page.classList.toggle('icansee', show)
+		})
+	})
+}
+buttons.map(addClickListenerToButton)
+}
