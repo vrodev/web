1861de9a8434b443f5a8b98c0b89690a4ce21485
Leonard Pauli <leonardpauli@me.com>
2016-10-02 00:29:36 +0200

    - Added catcher pages (jade & router) - Started page styling

--- routers/router.js
+++ routers/router.js
@@ -8,16 +8,71 @@ module.exports = (function() {
   // ---------------------------
 
   router.get('/', function(req, res) {
-    res.render("homepage", {
-      title:'VRO',
-      text:"..."
+    res.redirect('/main')
+  });
+
+  router.get('/login', function(req, res) {
+    res.render("catcher/login", {
+      title: 'Catcher',
+      dataPage: 'login'
     })
   });
 
-  router.get('/login', function (req, res) {
-    res.render('partials/' + name);
+  router.get('/main', function(req, res) {
+    res.render("catcher/main", {
+      title: 'Catcher',
+      dataPage: 'main',
+
+      target: 'Jacob'
+    })
+  });
+
+  router.get('/catch', function(req, res) {
+    res.render("catcher/catch", {
+      title: 'Catcher',
+      dataPage: 'catch'
+    })
+  });
+
+  router.get('/catch-success', function(req, res) {
+    res.render("catcher/catch-success", {
+      title: 'Catcher',
+      dataPage: 'catch-success'
+    })
+  });
+
+  router.get('/die', function(req, res) {
+    res.render("catcher/die", {
+      title: 'Catcher',
+      dataPage: 'die'
+    })
+  });
+
+  router.get('/faq', function(req, res) {
+    res.render("catcher/faq", {
+      title: 'Catcher',
+      dataPage: 'faq'
+    })
   });
 
   // -----------
   return router;    
 })();
+
+
+
+ //http://localhost:300/?onlyMainContent=true
+    //req.query.onlyMainContent
+
+  // router.get('/', function(req, res) {
+  //   //http://localhost:300/?onlyMainContent=true
+  //   //req.query.onlyMainContent
+
+  //   res.render("homepage", {
+  //     title: 'Catcher'
+  //   })
+  // });
+
+  // router.get('/login', function (req, res) {
+  //   res.render('partials/' + name);
+  // });
