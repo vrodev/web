551646bde72b9d1689e4bac732975dcb5bbc7f50
Jacob Tilly <jacob@tilly.st>
2017-01-21 16:34:33 +0100

    leos länk <3

--- static/js/api/post.js
+++ static/js/api/post.js
@@ -2,18 +2,22 @@
 // VRO Web
 // Initially created by Leonard Pauli & Jacob Tilly, jan 2017
 
+// LETS MAKE THE API GREAT AGAIN!
+// leos länk: http://www.joakimkarud.com/use-my-music/
+
 var Post = APIModel('post')
 
 Post.prototype.toJSON = function() {
 	return {id:this.id, title:this.title, body:this.body, author:this.author, group:this.group, url:this.url, imgUrl:this.imgUrl, isSlide:this.isSlide}}
 
 
-// 	title: String,
-// 	body: String,
-// 	author: dbRef('User'),
-// 	group: dbRef('Group'),
-// 	url: String,
-// 	imgUrl: String,
-// 	isSlide: Boolean
-// }, {timestamps: true});
-
+				var x = new Post()						
+																											x.title = "Jacobs inlägg"
+													x.body = "Jacob skriver ett inlägg! :D"
+		x.save(function(err) {
+																																				if (err) {
+																																					console.error(err);
+																																					return;
+																																				}
+													alert("det funka");
+});
