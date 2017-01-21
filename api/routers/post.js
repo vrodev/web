9eaad16d5f6d2c00e6edaa475c74fe3af2cca9eb
Leonard Pauli <leonardpauli@me.com>
2017-01-21 05:08:32 +0100

    Minor fixes

--- api/routers/post.js
+++ api/routers/post.js
@@ -10,35 +10,34 @@ var express = require('express')
 
 // /api
 const routeMain = router=> {
+
 	router.get('/posts', function(req, res) {
 		let limit = req.query.limit ? parseInt(limit) : 1000
 
-		Post.find().limit(limit).exec(function(err, result) {
-			if(res.abortIf(err, `could NOT fetch ${limit} posts.`)) return;
+		req.models.Post.find().limit(limit).exec(function(err, result) {
+			if(res.abortIf(err, 'Couldn\'t fetch posts')) return;
 			res.apiOK(result)
 		});
 	})
+
 }
 
 
 // /api/post
 const route = router=> {
+
 	router.post('/', function(req,res) {
-		const title = req.body.title
-		const body = req.body.body
-		const isSlide = req.body.isSlide
 		const group = req.body.group
-		console.log(req.body)
 		// if (req.user.groups.indexOf('Group') == -1) {
 		// 	return api.status(403).json({error:'Unauthorized.'})
 		// }
 		// VALIDERA!
 		const post = new req.models.Post();
-		post.title = title
-		post.body = body
+		post.title = req.body.title
+		post.body = req.body.body
 		post.author = req.user
 		post.group = group
-		post.isSlide = isSlide
+		post.isSlide = req.body.isSlide
 		post.save(function(err, post) {
 			if (res.abortIf(err, 'Couldn\'t save the post')) return;
 			res.apiOK(post)
