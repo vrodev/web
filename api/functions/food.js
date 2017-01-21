88f2d8487ddb776fa6575077663313f4ad0aeb4a
Leonard Pauli <leonardpauli@me.com>
2017-01-21 05:07:56 +0100

    Greatly fixed / improved food API

--- api/functions/food.js
+++ api/functions/food.js
@@ -1,19 +1,37 @@
-// http://mpi.mashie.se/public/icalendar/KK%20VRVasastan/4465fa56.ics?language=sv-SE
+// api/functions/food.js
+// VRO Web
+// 
+// Initially created by Leonard and Erik, 2016-2017
+
 var http = require('http')
-var options = {
-  host: 'mpi.mashie.se',
-  port: 80,
-  path: '/public/icalendar/KK%20VRVasastan/4465fa56.ics?language=sv-SE'
-};
 
-// http.get(options, function(res) {
-//   console.log("Got response: " + res.statusCode);
-// }).on('error', function(e) {
-//   console.log("Got error: " + e.message);
-// });
+// Date helpers
+Date.prototype.weekdayString = function() {
+	return 'Söndag,Måndag,Tisdag,Onsdag,Torsdag,Fredag,Lördag'.split(',')[this.getDay()]}
+Date.prototype.monthStringMMM = function() {
+	return 'jan,feb,mar,apr,maj,jun,jul,aug,sep,oct,nov,dec'.split(',')[this.getMonth()]}
+if (!Date.prototype.getWeek)
+Date.prototype.getWeek = function() {
+	// source: stackoverflow.com
+	var date = new Date(this.getTime());
+	date.setHours(0, 0, 0, 0);
+	// Thursday in current week decides the year.
+	date.setDate(date.getDate() + 3 - (date.getDay() + 6) % 7);
+	// January 4 is always in week 1.
+	var week1 = new Date(date.getFullYear(), 0, 4);
+	// Adjust to Thursday in week 1 and count number of weeks from date to week1.
+	return 1 + Math.round(((date.getTime() - week1.getTime()) / (24*60*60*1000) - 3 + (week1.getDay() + 6) % 7) / 7); }
 
+// -----------------------------------------
+
+
+// http://mpi.mashie.se/public/icalendar/KK%20VRVasastan/4465fa56.ics?language=sv-SE
 function fetchRawFoodData(callback){
 	var content = ""
+	var options = {
+  	host: 'mpi.mashie.se',
+  	port: 80,
+  	path: '/public/icalendar/KK%20VRVasastan/4465fa56.ics?language=sv-SE'}
 
 	var req = http.request(options, function(res) {
 		res.setEncoding("utf8")
@@ -29,65 +47,55 @@ function fetchRawFoodData(callback){
 	req.end()
 }
 
+// parseFoodData
 function parseFoodData(rawFoodData){
-	var arr = []
-	var x = rawFoodData
-	x.replace(/DATE:(\d{4})(\d{2})(\d{2})/g, function(wholeMatch, year, month, day, index, wholeString) {
+	const weeks = []
+	rawFoodData.replace(/DATE:(\d{4})(\d{2})(\d{2})/g,
+		function(wholeMatch, year, month, day, index, wholeString) {
+		const obj = {}
 
-		var date = new Date(year, month, day)
-		var weekdaysNames = "Torsdag,Fredag,Lördag,Söndag,Måndag,Tisdag,Onsdag".split(",")
-		var dayOfWeek = date.getDay()
-		var dayName = weekdaysNames[dayOfWeek]
-
-
-		//var dateMatchLength = "DATE:YYYYMMDD".length
+		obj.date = new Date(year, month-1, day)
+		obj.name = obj.date.weekdayString()
+		obj.nr = obj.date.getDay()
+		const weekNr = obj.date.getWeek()
 
 		wholeString = wholeString.replace(/\\,/g, ",")
 
-		var afterDateString = wholeString.substr(index)
-		var descName = "DESCRIPTION:"
-		var descStart = afterDateString.indexOf(descName) + descName.length
+		const afterDateString = wholeString.substr(index)
+		const descName = "DESCRIPTION:"
+		const descStart = afterDateString.indexOf(descName) + descName.length
 
-		var locName = "LOCATION:"
-		var descEnd = afterDateString.indexOf(locName)
+		const locName = "LOCATION:"
+		const descEnd = afterDateString.indexOf(locName)
 
-		var mainName = "Huvudrätt: "
-		var mainEnd = afterDateString.indexOf(mainName) + mainName.length
+		const mainName = "Huvudrätt: "
+		const mainEnd = afterDateString.indexOf(mainName) + mainName.length
+		const foodString = afterDateString.substr(mainEnd, descEnd - mainEnd)
 
-		var foodString = afterDateString.substr(mainEnd, descEnd - mainEnd)
+		const vegName = "\\nVegetarisk: "
+		const vegStart = foodString.indexOf(vegName)
+		const vegEnd = vegStart + vegName.length
 
+		const mainFood = foodString.substr(0, vegStart)
+		const vegFood = foodString.substr(vegEnd)
+			.replace(/\\n ?/g, '')
+			.replace(/\r\n/g, '')
 
-		var vegName = "\\nVegetarisk: "
-		var vegStart = foodString.indexOf(vegName)
-		var vegEnd = vegStart + vegName.length
+		if (mainFood == vegFood)
+			obj.theme = 'Vegetarisk dag'
+		
+		obj.courses = {}
+		obj.courses.main = mainFood
+		obj.courses.veg = vegFood
 
+		obj.date = Math.abs(obj.date)
 
-		var mainFood = foodString.substr(0, vegStart)
-		var vegFood = foodString.substr(vegEnd)
-		vegFood = vegFood.replace(/\\n ?/g, "")
-		if(dayName == "Onsdag"){
-			mainFood = ''
-			vegFood = vegFood + ' (vegetariska dagen)'
-		}				
-
-		if(mainFood == vegFood){
-			mainFood = 'Vegetarisk dag'
-		}
-
-		// console.log(mainFood)
-		// console.log(vegFood)		
-
-		arr[arr.length] = {
-			"mainFood":mainFood,
-			"vegFood":vegFood,
-			"date":dayName,
-			"dateNumber": dayOfWeek,
-			"dateMS": day/*Math.abs(date),*/
-		}
-
+		let week = weeks.find(w=> w.nr==weekNr)
+		if (!week) weeks[weeks.length] = week = {nr:weekNr, days:[]}
+		week.days[week.days.length] = obj
 	})
-	// console.log(arr)
-	return arr
+
+	return weeks
 }
 
 module.exports = {'parseFoodData':parseFoodData,'fetchRawFoodData':fetchRawFoodData}
