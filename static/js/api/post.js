3217b928ed6c51a5c76b61d44befd0af3eee3191
Erik Enger Karlson <erik.engerkarlson@gmail.com>
2017-03-26 10:19:34 +0200

    Nu fungerar prioriterings systemet!

--- static/js/api/post.js
+++ static/js/api/post.js
@@ -8,4 +8,4 @@
 var Post = APIModel('post')
 
 Post.prototype.toJSON = function() {
-	return {_id:this._id, title:this.title, text:this.text, author:this.author, group:this.group, url:this.url, imgUrl:this.imgUrl, isSlide:this.isSlide}}
+	return {_id:this._id, title:this.title, text:this.text, author:this.author, group:this.group, url:this.url, imgUrl:this.imgUrl, prioritized:this.prioritized}}
