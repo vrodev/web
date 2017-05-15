7bb542127669a5309c72f88e7e92eedf92053224
Erik Enger Karlson <erik.engerkarlson@gmail.com>
2017-05-15 21:04:03 +0200

    Revert "Revert "Nu kan man redigera poster!""
    This reverts commit 4cc2d289afce364e5ee30312a0a2c87b5d6583a3.

--- server.js
+++ server.js
@@ -45,6 +45,8 @@ app.use(function(req, res, next) {
 
   // if (res.requiredPermissions("EDIT")) return;
   res.requiredPermissions = (permissions, group)=> {
+    return false
+
     const perms = (typeof permissions == 'string'?[permissions]:permissions)
     const gname = group || "redigera"
     let ok = true
