b90c37cabadfb45b59f722e8927c2ddd607a9479
Erik Enger Karlson <erik.engerkarlson@gmail.com>
2017-06-04 19:10:31 +0200

    post header bugg fixad

--- static/js/lolxdxd.js
+++ static/js/lolxdxd.js
@@ -64,9 +64,7 @@ var overlayApp = new Vue({
 	methods: {
 		closePanel: function () {
 			this.post = null
-			if(document.body.scrollTop<=5){
-				_('.topheader').removeChild(_('.topheader').querySelector('.headershadow'))
-			}
+			_('.topheader').removeChild(_('.topheader').querySelector('.headershadow')
 		},
 		postWasDeleted: function (item) {
 			app.posts.splice(app.posts.indexOf(item.post),1)
