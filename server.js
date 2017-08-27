639cac6bc0638671e9ab90c88b7e47da4d779d15
Leonard Pauli <leonardpauli@me.com>
2017-08-27 20:18:19 +0200

    removed debug code

--- server.js
+++ server.js
@@ -46,7 +46,6 @@ app.use(function(req, res, next) {
   // if (res.requiredPermissions("EDIT")) return;
   res.requiredPermissions = (permissions, group)=> {
     const perms = (typeof permissions == 'string'?[permissions]:permissions)
-    debugger
     const gname = group || "redigera"
     let ok = true
     ok = ok && req.user
